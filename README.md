514de64217ac4e1e1231c58057fb68ecf17af8ce
Jade <58603879+IchBinJade@users.noreply.github.com>
2024-11-23 17:10:09 +0000

    Initial commit of existing solutions (#1)
    * Create __init__.py
    
    * Create dayX-template.py
    
    * Update README.md
    
    * Create utils.py
    
    * Create 2023/day01.py
    
    * Add day02 and day04
    
    Day 3 purposefully skipped as not started yet
    
    * Update day04.py

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# advent-of-code-python
-Advent of Code Solutions - Python
+# Advent of Code Solutions
+
+WIP: This will be the main repo for all python solutions. I have some other repos for past years that I'll work to merge in.
+
+## 2024
+
+Coming soon!
+
+## 2023
+
+[Day 01](/2023/day01.py) - Part 1 & 2
+
+[Day 02](/2023/day02.py) - Part 1 & 2
+
+Day 03
+
+[Day 04](/2023/day04.py) - Part 1
---
+++ dayX-template.py
@@ -0,0 +1,36 @@
+"""
+Author: IchBinJade
+Date  : YYYY-MM-DD
+AoC Day X - https://adventofcode.com/20YY/day/X
+"""
+
+import sys
+import os
+import time
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+
+from utils import get_list_from_file
+
+def part_one(input):
+    pass
+
+
+def part_two(input):
+    pass
+
+
+
+if __name__ == "__main__":
+    t1 = time.time()
+
+    # Get input data
+    input_data = get_list_from_file(1, 2023)
+
+    # Get solutions
+    print(f"Part 1 = {part_one(input_data)}")
+    print(f"Part 2 = {part_two(input_data)}")
+
+    # Calc execution time
+    t2 = time.time()
+    print(f"Executed in {t2 - t1:0.4f} seconds")
