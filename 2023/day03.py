cb3d725b3f67f283c8c875546110a248c1a66b85
Jade Hurst <jadeh>
2024-11-24 18:01:35 +0000

    2023 Day 3 - Part one works with test input

--- 2023/day03.py
+++ 2023/day03.py
@@ -13,10 +13,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
 from utils import get_list_from_file
 
+DIGITS = r"\d+"
 SYMBOLS = r"[^.\d]"
 NEIGHBOURS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
 def extract_whole_num(row, char_pos, input):
+    # Save start pos to get end pos later
+    start_pos = char_pos
     number_str = ""
     # Keep shifting along until we stop getting a digit
     while char_pos < len(input[row]) and input[row][char_pos].isdigit():
@@ -24,7 +27,8 @@ def extract_whole_num(row, char_pos, input):
         char_pos += 1
 
     if number_str:
-        return int(number_str)
+        end_pos = char_pos - 1
+        return int(number_str), start_pos, end_pos
     
     return None
 
@@ -32,28 +36,42 @@ def extract_whole_num(row, char_pos, input):
 def get_number_list(input):
     numbers_pos = []
     for row in range(len(input)):
-        for char_pos in range(len(input[row])):
-            if input[row][char_pos].isdigit():
-                number = extract_whole_num(row, char_pos, input)
-                if number is not None:
-                    numbers_pos.append((number, row, char_pos))
+        for match in re.finditer(DIGITS, input[row]):
+            start_pos = match.start(0)
+            end_pos = match.end(0) - 1
+            number = int(match.group(0))
+            numbers_pos.append((number, row, start_pos, end_pos))
+
+        # for char_pos in range(len(input[row])):
+        #     if input[row][char_pos].isdigit():
+        #         number, start_pos, end_pos = extract_whole_num(row, char_pos, input)
+        #         if number is not None:
+        #             numbers_pos.append((number, row, start_pos, end_pos))
 
     return numbers_pos
 
 
 def part_one(input):
     total = 0
+    counted_pos = set()
     numbers_pos = get_number_list(input)
-    print(f"numbers_pos >>> {numbers_pos}")
     # Check for symbols adjacent to each number
-    for number, row, char_pos in numbers_pos:
-        for x, y in NEIGHBOURS:
-                    nb_row, nb_col = row + x, char_pos + y
+    for number, row, start_pos, end_pos in numbers_pos:
+        for col in range(start_pos, end_pos + 1):
+            for x, y in NEIGHBOURS:
+                    nb_row, nb_col = row + x, col + y
                     # Check neighbour within bounds
-                    if 0 <= nb_row < len(input) and 0 <= nb_col < len(input[row]):
-                        if re.match(SYMBOLS, input[row][char_pos]):
+                    if 0 <= nb_row < len(input) and 0 <= nb_col < len(input[nb_row]):
+                            # Debugging: Show position of current neighbour being checked
+                            # print(f"counted_pos content = {counted_pos}")
+                            # print(f"Checking neighbour at ({nb_row}, {nb_col}) for number {number}")
+                            # print(f"neighbour content = {input[nb_row][nb_col]}")
+                        if re.match(SYMBOLS, input[nb_row][nb_col]) and (number, start_pos, end_pos) not in counted_pos:
                             total += number
-                            break # No need to check other neighbours, already counted
+                            counted_pos.add((number, start_pos, end_pos))
+                            #counted_pos.add((nb_row, nb_col))
+                            #print(f"Adding number {number} due to symbol at ({nb_row}, {nb_col})")
+                            break  # Once a valid symbol is found, we can stop checking further neighbours
 
     return total
 
