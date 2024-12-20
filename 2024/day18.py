e5429270bd8c61f85cbe912fe8bdd24c4b39c6a4
Jade Hurst <jadeh>
2024-12-20 23:02:18 +0000

    Solved: 2024 Day 18 - Part 2

--- 2024/day18.py
+++ 2024/day18.py
@@ -4,6 +4,7 @@ Date  : 2024-12-20
 AoC 2024 Day 18 - https://adventofcode.com/2024/day/18
 
 Utilises: BFS
+Execution time:  ~8s
 """
 
 import sys
@@ -36,28 +37,56 @@ def find_path(grid):
     return None
 
 
+def find_trouble_coord(grid):
+    end_pos = (70, 70)
+    rows, cols = len(grid), len(grid[0])
+    
+    visited = set((0,0))
+    queue = deque([(0, 0)])
+    while queue:
+        curr_row, curr_col = queue.popleft()
+        if (curr_row, curr_col) == end_pos:
+            # Exit has been found, move to the next coord
+            return True
+        for next_row, next_col in [(curr_row + 1, curr_col), (curr_row, curr_col + 1), (curr_row - 1, curr_col), (curr_row, curr_col - 1)]:
+            # Bounds check
+            if 0 <= next_row < rows and 0 <= next_col < cols:
+                if grid[next_row][next_col] != "#" and (next_row, next_col) not in visited:
+                    visited.add((next_row, next_col))
+                    queue.append((next_row, next_col))
+                        
+    return False
+
+
 def part_one(data_input):
     # Create grid and plot coords of first
     grid = [list("." for _ in range(71)) for _ in range(71)]
     coord_list = [tuple(map(int, coord.split(","))) for coord in data_input]
     for x, y in coord_list[:1024]:
         grid[y][x] = "#"
-    # for row in grid:
-    #     print(*row, sep="")
+
     return find_path(grid)
 
-def part_two(data_input):
-    pass
 
-TEST_INPUT = ['5,4', '4,2', '4,5', '3,0', '2,1', '6,3', '2,4', '1,5', '0,6', '3,3', '2,6', '5,1', '1,2', '5,5', '2,5', '6,5', '1,4', '0,4', '6,4', '1,1', '6,1', '1,0', '0,5', '1,6', '2,0']
+def part_two(data_input):
+    # Create grid and plot coords of first
+    grid = [list("." for _ in range(71)) for _ in range(71)]
+    coord_list = [tuple(map(int, coord.split(","))) for coord in data_input]
+    for x, y in coord_list[:12]:
+        grid[y][x] = "#"
+    
+    # Then go through the remaining coords until the path is blocked
+    for next_x, next_y in coord_list[12:]:
+        grid[next_y][next_x] = "#"
+        if find_trouble_coord(grid) == False:
+            return ",".join((str(next_x), str(next_y)))
+
 
 if __name__ == "__main__":
     t1 = time.time()
 
     # Get input data
     input_data = get_list_from_file(18, 2024)
-    
-    #input_data = TEST_INPUT
 
     # Get solutions
     print(f"Part 1 = {part_one(input_data)}")
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Main repo for Advent of Code solutions in Python
 
 ## Years
 
-[2024](#2024) - In Progress (17.5/25)
+[2024](#2024) - In Progress (18/25)
 
 [2023](#2023) - In Progress (9.5/25)
 
@@ -52,7 +52,7 @@ Main repo for Advent of Code solutions in Python
 
 [Day 17](/2024/day17.py) - Part 1 & 2
 
-[Day 18](/2024/day18.py) - Part 1
+[Day 18](/2024/day18.py) - Part 1 & 2
 
 </details>
 
